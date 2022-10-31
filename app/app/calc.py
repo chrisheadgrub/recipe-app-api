0bc14b3b1e5fa514331a3516d361196d878330ac
kloftis <kloftis@jhu.edu>
2022-10-31 08:48:49 -0400

    Configured docker compose and checks to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,11 @@
 Calculator functions
 """
 
+
 def add(x, y):
-  """ Add x and y, return result """
-  return x + y
+    """ Add x and y, return result """
+    return x + y
+
 
 def subtract(x, y):
-  return y - x
+    return y - x
